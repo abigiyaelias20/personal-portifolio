495c2d5e9b027aa0a5ca6d5ea0bbffef24130a0e
abigiyaelias20 <abigiyaelias20.git.com>
2025-03-04 23:37:50 +0300

    New commit message

---
+++ abigiya--7--main/abigiya/assets/js/script.js
@@ -0,0 +1,115 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+  let cvFilePath = 'assets/file/CV.pdf'; 
+
+  let downloadLink = document.createElement('a');
+  
+  downloadLink.href = cvFilePath;
+
+  downloadLink.setAttribute('download', 'CV.pdf'); 
+
+  document.body.appendChild(downloadLink);
+
+  downloadLink.click();
+
+  document.body.removeChild(downloadLink);
+});
+
+// Select all about-card elements
+const aboutCards = document.querySelectorAll('.about-card');
+
+// Add event listeners for hover
+aboutCards.forEach(card => {
+  card.addEventListener('mouseenter', function() {
+    this.classList.add('glow');
+  });
+
+  card.addEventListener('mouseleave', function() {
+    this.classList.remove('glow');
+  });
+});
+
+
+
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
